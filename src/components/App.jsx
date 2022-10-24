c94cab4f7830decad881eeb1150eb88b5e50ffd0
just-jhorge <sarpognk247@gmail.com>
2022-10-24 10:58:33 +0100

    further updates and changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import NewsList from '../pages/NewsList'
-import Business from '../pages/Business'
-import Technology from '../pages/Technology'
-import Sports from '../pages/Sports'
-import Entertainment from '../pages/Entertainment'
-import Navbar from './Navbar'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NewsList from "../pages/NewsList";
+import Business from "../pages/Business";
+import Technology from "../pages/Technology";
+import Sports from "../pages/Sports";
+import Entertainment from "../pages/Entertainment";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Navbar/>
+        <Header />
         <Routes>
-          <Route path='/' element={<NewsList/>} />
-          <Route path='/business' element={<Business/>} />
-          <Route path='/technology' element={<Technology/>} />
-          <Route path='/sports' element={<Sports/>} />
-          <Route path='/entertainment' element={<Entertainment/>} />
+          <Route path="/" element={<NewsList />} />
+          <Route path="/business" element={<Business />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/sports" element={<Sports />} />
+          <Route path="/entertainment" element={<Entertainment />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Entertainment.jsx
+++ src/pages/Entertainment.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NewsItem from "../components/NewsItem";
+import _ from "lodash";
 
 const Entertainment = () => {
-  return (
-    <div>Entertainment</div>
-  )
-}
+  const NEWSAPI = "fa21c0eb075a4b28bfc19320dad77ff6";
+  const [articles, setArticles] = useState([]);
 
-export default Entertainment
+  useEffect(() => {
+    const getArticles = async () => {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=entertainment&apiKey=${NEWSAPI}`
+      );
+      setArticles(response.data.articles);
+      console.log(response.data.articles);
+    };
+
+    getArticles();
+  }, []);
+
+  return (
+    <div className="w-full md:w-[85%] mx-auto flex flex-wrap gap-5 md:gap-10 mt-20">
+      {articles &&
+        articles.map((article) => (
+          <NewsItem
+            date={`${new Date(article.publishedAt)
+              .toLocaleString("default", { month: "short" })
+              .toUpperCase()}, ${new Date(article.publishedAt).getFullYear()}`}
+            urlToImage={article.urlToImage}
+            description={_.truncate(article.description, { length: 60 })}
+            title={article.title}
+            articleLink={article.url}
+            key={article.url}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Entertainment;
